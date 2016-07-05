1906492ba4b4c376430f3a0945b5aaaa7f1eafbc
Joseungwan <wh7923@naver.com>
2016-07-05 18:49:03 +0900

    Filter_vol1

--- client/js/controllers/TodomvcCtrl.js
+++ client/js/controllers/TodomvcCtrl.js
@@ -35,8 +35,25 @@ angular.module('todomvc')
                 id: newId,
                 title: todoTitle,
                 completed: false
-            };
+            }
             
             $scope.todos.push(newTodo);
         };
+
+        $scope.$watch('status', function () {
+            if ($scope.status === 'completed') {        // Completed 클릭시
+                $scope.statusFilter = {completed: true};  // 필터를 설정한다.
+            } else if ($scope.status === 'active') {    // Active 클릭시
+                $scope.statusFilter = {completed: false}; // 필터를 설정한다.
+            } else {                                    // All 클릭시
+                $scope.statusFilter = {};                 // 필터를 해제한다.
+            }
+        });
+
+        $scope.clearCompleted = function () {
+            var incompleteTodos = $scope.todos.filter(function (todo) {
+                return !todo.completed;
+            });
+            angular.copy(incompleteTodos, $scope.todos);
+        }
     });
