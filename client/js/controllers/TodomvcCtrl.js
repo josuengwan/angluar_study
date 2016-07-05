dd54a8458f0e65609d02cefd00ef7aaffd09dfec
Joseungwan <wh7923@naver.com>
2016-07-05 18:26:25 +0900

    add_delete_vol1

--- client/js/controllers/TodomvcCtrl.js
+++ client/js/controllers/TodomvcCtrl.js
@@ -12,4 +12,31 @@ angular.module('todomvc')
             title:'lovelove',
             completed: true
         }];
+
+        $scope.remove = function (id) {
+            if(!id) return;
+
+            var deletedTodoIdx = $scope.todos.findIndex(function (todo) {
+                return todo.id === id;
+            });
+
+            if(deletedTodoIdx === -1) return;
+             $scope.todos.splice((deletedTodoIdx),1);
+        }
+        
+        $scope.addTodo = function (todoTitle) {
+            todoTitle = todoTitle.trim();
+            if(!todoTitle) return;
+            
+            var newId = !$scope.todos.length ?
+                1 : $scope.todos[$scope.todos.length-1].id+1;
+            
+            var newTodo = {
+                id: newId,
+                title: todoTitle,
+                completed: false
+            };
+            
+            $scope.todos.push(newTodo);
+        };
     });
